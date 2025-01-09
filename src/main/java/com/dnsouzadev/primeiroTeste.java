b4cae9afb1da048f632098ff9ec9aaf787d0b790
Daniel Souza <workdndsza@gmail.com>
2025-01-09 14:48:34 -0300

    primeiro teste

---
+++ src/main/java/com/dnsouzadev/primeiroTeste.java
@@ -0,0 +1,26 @@
+package com.dnsouzadev;
+
+public class primeiroTeste {
+
+//    int INDICE = 13, SOMA = 0, K = 0;
+//
+//    Enquanto K < INDICE faça {
+//        K = K + 1;
+//        SOMA = SOMA + K;
+//    }
+//
+//    Imprimir(SOMA);
+
+    public static void main(String[] args) {
+        int INDICE = 13, SOMA = 0, K = 0;
+
+        while (K < INDICE) {
+            K = K + 1;
+            SOMA = SOMA + K;
+        }
+
+        System.out.println(SOMA);
+    }
+
+
+}
